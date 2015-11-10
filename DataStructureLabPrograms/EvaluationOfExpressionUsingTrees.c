4aba4d502a04249a129897fd9b04426b9742d302
Saurav Sahu <sauravsahu.12@live.com>
2015-11-10 16:45:55 +0530

    Had to use a stack in the end for Creating the Binary Tree. Works for all the expressions I've typed in so far.

--- DataStructureLabPrograms/EvaluationOfExpressionUsingTrees.c
+++ DataStructureLabPrograms/EvaluationOfExpressionUsingTrees.c
@@ -1,47 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
 #include <malloc.h>
 #define SIZE 40
-
 struct NODE
 {
 	double value;
 	char symbol;
 	struct NODE *lchild, *rchild;
 };	typedef struct NODE *node;
-
-node CreateNode();
-void Insert(node *, char);
-void CreateTree(node *, char[]);
-double Evaluate(node *);
-void Reverse(char[]);
-
-void main()
+void Push(node **stack, int *top, node *temp)
 {
-	char postfix[SIZE], temp[SIZE];
-	node root = NULL;
-	double ans;
-	printf("\nEnter the postfix expression: ");
-	scanf("%s", postfix);
-	strcpy(temp, postfix);
-	Reverse(temp);
-	CreateTree(&root, temp);
-	ans = Evaluate(&root);
-	printf(" = %.4lf", ans);
+	(*stack)[++(*top)] = *temp;
 }
-void Reverse(char string[])
+node Pop(node **stack, int *top)
 {
-	int i, j; char t;
-	for (i = 0, j = strlen(string) - 1; i <= strlen(string) / 2; ++i, --j)
-	{
-		t = string[i];
-		string[i] = string[j];
-		string[j] = t;
-	}
+	return(*stack)[(*top)--];
 }
 node CreateNode()
 {
@@ -49,38 +25,48 @@ node CreateNode()
 	temp->lchild = temp->rchild = NULL;
 	return temp;
 }
-void Insert(node *n, char symbol)
+void ReadData(node *n)
 {
-	if (*n == NULL)
+	printf("\nEnter value for operand (%c): ", (*n)->symbol);
+	scanf("%lf", &((*n)->value));
+}
+int IsOperator(char ch)
+{
+	switch (ch)
 	{
-		*n = CreateNode();
-		(*n)->symbol = symbol;
-		if (isalpha(symbol))
-		{
-			printf("\nEnter value for operand (%c): ", symbol);
-			scanf("%lf", &((*n)->value));
-		}
-	}
-	else
-	{
-		if ((*n)->rchild == NULL)
-			Insert(&((*n)->rchild), symbol);
-		else
-			Insert(&((*n)->lchild), symbol);
+	case '-':
+	case '+':
+	case '/':
+	case '*':
+	case '^': return 1;
+	default: return 0;
 	}
 }
-void CreateTree(node *root, char exp[])
+node CreateTree(char exp[])
 {
-	int i;
+	node *stack = malloc(sizeof(struct NODE)*strlen(exp)), temp;
+	int i, top = -1;
 	for (i = 0; i < strlen(exp); ++i)
 	{
-		Insert(root, exp[i]);
+		temp = CreateNode();
+		temp->symbol = exp[i];
+		if (IsOperator(exp[i]) == 0)
+		{
+			ReadData(&temp);
+		}
+		else
+		{
+			(temp)->rchild = Pop(&stack, &top);
+			(temp)->lchild = Pop(&stack, &top);
+		}
+		Push(&stack, &top, &temp);
 	}
+	return Pop(&stack, &top);
 }
 double Evaluate(node *n)
 {
 	if ((*n)->rchild == NULL)
-	//Any node will not have a single child
+		//Any node will not have a single child
 	{
 		return ((*n)->value);
 	}
@@ -94,3 +80,16 @@ double Evaluate(node *n)
 	default:  return 0;
 	}
 }
+void main()
+{
+	char postfix[SIZE];
+	
+	node root = NULL;
+	double ans;
+	printf("\nEnter the postfix expression: ");
+	scanf("%s", postfix);
+	root = CreateTree(postfix);
+	ans = Evaluate(&root);
+	printf("\nResult = %.2lf\n", ans);
+}
+
